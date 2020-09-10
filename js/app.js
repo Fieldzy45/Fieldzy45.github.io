db794a3260e03fe01194746832089badafb71165
Fieldzy45 <71046827+Fieldzy45@users.noreply.github.com>
2020-09-09 22:12:51 -0400

    FirstSite
    my first attempt at a website

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+
+//single line comment 
+
+/* multiple 
+line 
+comment */
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+let x=0;
+console.log();
+'use strict'
+/*
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "+"+x;
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+    x=x+1;
+    button.innerHTML="+"+ x;
+  
+});
+
+ Read 
+
+https://css-tricks.com/use-button-element/
+*/
